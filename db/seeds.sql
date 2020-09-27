ac432dc5a52a375b8e1b5b5710dc30e99f87afb7
Manny Lou <mavkio1@gmail.com>
2020-09-27 18:45:03 -0400

    Project Complete.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db
+
+INSERT INTO burgers (burger_name) VALUES ('Chris Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Your Lose Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Shopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whatadishuger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Stackandshank burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Jenbuburger');
